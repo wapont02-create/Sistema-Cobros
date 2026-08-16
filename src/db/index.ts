64b507e42ef3fe2fae9e97300aa171e6a73a04c8
wapont02-create <wapont02@gmail.com>
2026-08-16 10:56:07 -0400

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,1 +1,7 @@
+import { drizzle } from 'drizzle-orm/sqlite-cloud';
+import { SQLiteCloudClient } from '@sqlitecloud/drivers';
 
+const connectionString = process.env.DATABASE_URL || "sqlitecloud://cza41vhuvk.g5.sqlite.cloud:8860/pos_db?apikey=fhKwclvjolNckgr9aCbyR44haiLLdrm6OhQibbXb4Zg";
+
+const client = new SQLiteCloudClient(connectionString);
+export const db = drizzle(client);
