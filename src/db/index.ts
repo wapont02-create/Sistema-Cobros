9c609e8d1254787cdf0ba20b94750187256f70e8
wapont02-create <wapont02@gmail.com>
2026-08-16 13:54:07 -0400

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
-import { SQLiteCloudClient } from '@sqlitecloud/drivers';
+import { Database } from '@sqlitecloud/drivers';
 
 const connectionString = process.env.DATABASE_URL || "sqlitecloud://cza41vhuvk.g5.sqlite.cloud:8860/pos_db?apikey=fHKwclvJoINckgr9aCbyR44haillDrm60hQibbXb4Zg";
 
-const client = new SQLiteCloudClient(connectionString);
+const dbConn = new Database(connectionString);
 
 export const db = drizzle(async (sql, params, method) => {
   try {
-    const response = await client.sql(sql);
+    const response = await dbConn.sql(sql);
     return { rows: response || [] };
   } catch (error) {
     console.error('Error al ejecutar la consulta:', error);
