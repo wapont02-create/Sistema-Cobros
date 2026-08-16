2773fee47bfefceff7b107d5e9dc7a89130f43d1
wapont02-create <wapont02@gmail.com>
2026-08-16 13:49:12 -0400

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,16 @@
-import { drizzle } from 'drizzle-orm/sqlite-cloud';
+import { drizzle } from 'drizzle-orm/sqlite-proxy';
 import { SQLiteCloudClient } from '@sqlitecloud/drivers';
 
-const connectionString = process.env.DATABASE_URL || "sqlitecloud://cza41vhuvk.g5.sqlite.cloud:8860/pos_db?apikey=fhKwclvjolNckgr9aCbyR44haiLLdrm6OhQibbXb4Zg";
+const connectionString = process.env.DATABASE_URL || "sqlitecloud://cza41vhuvk.g5.sqlite.cloud:8860/pos_db?apikey=fHKwclvJoINckgr9aCbyR44haillDrm60hQibbXb4Zg";
 
 const client = new SQLiteCloudClient(connectionString);
-export const db = drizzle(client);
+
+export const db = drizzle(async (sql, params, method) => {
+  try {
+    const response = await client.sql(sql);
+    return { rows: response || [] };
+  } catch (error) {
+    console.error('Error al ejecutar la consulta:', error);
+    return { rows: [] };
+  }
+});
