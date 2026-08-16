398379d55b15ff64c96e31830b26add8de5b4311
wapont02-create <wapont02@gmail.com>
2026-08-16 12:01:21 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'node_modules/@types/react' // O importar de 'react' segun prefieras
 
+export const metadata: Metadata = {
+  title: 'Sistema de Cobros POS',
+  description: 'Punto de venta y control de inventario',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
