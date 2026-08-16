7d130457dad331f4104b6f343c7567071b4c2418
wapont02-create <wapont02@gmail.com>
2026-08-16 12:02:28 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import type { Metadata } from 'node_modules/@types/react' // O importar de 'react' segun prefieras
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Sistema de Cobros POS',
