1f4a02f8876f8c10ddfda0f0ee98c6e4e71a3fcd
wapont02-create <wapont02@gmail.com>
2026-08-22 08:24:04 -0400

    Update route.ts

--- src/app/api/cash/route.ts
+++ src/app/api/cash/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { runQuery } from '../../../../../db/client';
+import { runQuery } from '../../../../db/client';
 
 /**
  * ============================================================
